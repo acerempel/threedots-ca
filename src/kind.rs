d003e3faedc7d2fd6baf1416357b397cd1db8db7
Alan Rempel <alan.rempel@gmail.com>
2020-09-30 18:19:25 -0400

    Move discern_file_kind to kind.rs
    It seemed to belong there. Would rather just have main() in main.rs.

--- src/kind.rs
+++ src/kind.rs
@@ -1,8 +1,9 @@
+use relative_path::{RelativePath,RelativePathBuf};
+use anyhow::Result;
+
 /// What kind of file? Does it contain content that we must process and
 /// output; is it a template that we must load and let Tera take care of;
 /// or is it an asset that we just copy over?
-use relative_path::RelativePathBuf;
-
 pub enum FileKind {
     Content(ContentKind, RelativePathBuf, URL),
     Asset(RelativePathBuf),
@@ -16,3 +17,34 @@ pub enum ContentKind {
     Markdown,
     Html,
 }
+
+/// Look at a file path and figure out, based on the file
+/// extension(s) or lack thereof, how we should treat it.
+pub fn discern_file_kind(input_path: &RelativePath) -> Result<FileKind> {
+    let input_ext_opt = input_path.extension();
+    let input_stem = input_path.file_stem();
+    if let Some(stem) = input_stem {
+        let input_ext = input_ext_opt.unwrap();
+        let input_parent_dir = input_path.parent();
+        let index_html = || {
+            if stem == "index" {
+                input_parent_dir
+                    .map(|dir| dir.join(RelativePath::new("index.html")))
+                    .unwrap_or(RelativePath::new("index.html").to_relative_path_buf())
+            } else {
+                input_parent_dir
+                    .map(|dir| dir.join(stem).join(RelativePath::new("index.html")))
+                    .unwrap_or_else(|| RelativePath::new(stem).join(RelativePath::new("index.html")))
+            }
+        };
+        let content_url = || input_parent_dir
+            .map(|dir| format!("/{}/{}/", dir, stem))
+            .unwrap_or_else(|| format!("/{}/", stem));
+        match input_ext {
+            "md" => Ok( FileKind::Content(ContentKind::Markdown, index_html(), content_url()) ),
+            "html" => Ok( FileKind::Content(ContentKind::Html, index_html(), content_url()) ),
+            _ => Ok( FileKind::Asset(input_path.to_owned()) ),
+        }
+    } else { Ok( FileKind::Asset(input_path.to_owned()) ) }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 extern crate anyhow;
 
 use crate::post::Post;
-use relative_path::{RelativePath, RelativePathBuf};
+use relative_path::RelativePathBuf;
 use anyhow::Result;
 
 mod kind;
@@ -22,36 +22,6 @@ mod path;
 use path::*;
 
 use std::fs;
-
-/// Look at a file path and figure out, based on the file
-/// extension(s) or lack thereof, how we should treat it.
-fn discern_file_kind(input_path: &RelativePath) -> Result<FileKind> {
-    let input_ext_opt = input_path.extension();
-    let input_stem = input_path.file_stem();
-    if let Some(stem) = input_stem {
-        let input_ext = input_ext_opt.unwrap();
-        let input_parent_dir = input_path.parent();
-        let index_html = || {
-            if stem == "index" {
-                input_parent_dir
-                    .map(|dir| dir.join(RelativePath::new("index.html")))
-                    .unwrap_or(RelativePath::new("index.html").to_relative_path_buf())
-            } else {
-                input_parent_dir
-                    .map(|dir| dir.join(stem).join(RelativePath::new("index.html")))
-                    .unwrap_or_else(|| RelativePath::new(stem).join(RelativePath::new("index.html")))
-            }
-        };
-        let content_url = || input_parent_dir
-            .map(|dir| format!("/{}/{}/", dir, stem))
-            .unwrap_or_else(|| format!("/{}/", stem));
-        match input_ext {
-            "md" => Ok( FileKind::Content(ContentKind::Markdown, index_html(), content_url()) ),
-            "html" => Ok( FileKind::Content(ContentKind::Html, index_html(), content_url()) ),
-            _ => Ok( FileKind::Asset(input_path.to_owned()) ),
-        }
-    } else { Ok( FileKind::Asset(input_path.to_owned()) ) }
-}
 
 use std::fs::File;
 
