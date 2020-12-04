b7efbf2a243eed5e66f97cd6864740d0ceabb796
Alan Rempel <alan.rempel@gmail.com>
2020-12-04 09:50:26 -0500

    Terser, separate css, something else

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import { string } from "rollup-plugin-string";
+import replace from "@rollup/plugin-replace";
+import { terser } from "rollup-plugin-terser";
 
 export default {
   input: 'scripts/main.js',
@@ -9,5 +11,8 @@ export default {
   },
   plugins: [
     resolve(),
-    string({ include: "bontent/base.css" })]
+    string({ include: "bontent/base.css" }),
+    replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
+    terser()
+  ]
 };
--- scripts/note.js
+++ scripts/note.js
@@ -1,17 +1,23 @@
 import {LitElement, html, css, unsafeCSS} from 'lit-element';
 import baseStyles from '../bontent/base.css';
+import { createPopper } from '@popperjs/core/lib/popper-lite';
+import flip from '@popperjs/core/lib/modifiers/flip';
+import preventOverflow from '@popperjs/core/lib/modifiers/preventOverflow';
+import offset from '@popperjs/core/lib/modifiers/offset';
 
 class Note extends LitElement {
   static get properties() {
     return {
-      open: { type: Boolean },
-      id: { type: String }
+      open: { type: Boolean, reflect: true },
+      id: { type: String },
+      popup: { type: Object, attribute: false }
     };
   }
   constructor() {
     super();
     this.open = false;
     this.id = Math.random().toString(); // a bit hacky
+    this.popup = null;
   }
   static get styles() {
     return css`
@@ -19,31 +25,55 @@ class Note extends LitElement {
       .note-toggle {
         font-size: calc(var(--base-font-size)*0.9375);
         color: var(--text-colour-highlight);
+        display: inline-block;
+        border: none;
+        background-color: transparent;
       }
       .note-content {
-        color: var(--text-colour-lighter);
+        background-color: var(--background-colour-lighter);
+        border-radius: calc(var(--base-line-height)*0.125);
+        border: calc(var(--base-line-height)*0.0625) solid var(--text-colour-normal);
+        padding: calc(var(--base-line-height)*0.25) calc(var(--base-line-height)*0.375);
+        width: 40ch;
+        max-width: 80vw;
       }
     `;
   }
   render() {
     return html`
-      <span
+      <button
         class="note-toggle"
-        role="button"
         tabindex="0"
         title="Show/hide note"
         aria-label="Show/hide note"
         aria-expanded="${ this.open }"
         aria-pressed="${ this.open }"
         aria-controls="${ this.id }"
-        @click="${ (_) => { this.open = !this.open } }"
-      >[${ this.open ? '−' : '+' }]</span>
+        @click="${ this._toggle }"
+      >✱</button>
       <small
         ?hidden=${ !this.open } id="${ this.id }"
         role="note"
         class="smaller note-content"
       ><slot></slot></small>
     `;
+  }
+  _toggle(_ev) {
+    if (this.open) {
+      this.open = false;
+      this.popup.destroy();
+    } else {
+      this.open = true;
+      let toggle = this.shadowRoot.querySelector(".note-toggle");
+      let note = this.shadowRoot.querySelector(".note-content");
+      let main = document.querySelector("main");
+      offset.options = { offset: [0, 5] };
+      preventOverflow.options = { boundary: main };
+      this.popup = createPopper(toggle, note, {
+        modifiers: [flip, preventOverflow, offset],
+        placement: "bottom"
+      });
+    };
   }
 }
 
