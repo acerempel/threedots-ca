7ebc49816d6e7043fbde338bd43e7f46db0e3cc9
Alan Rempel <alan.rempel@gmail.com>
2021-01-29 21:06:54 -0500

    Give the link to the atom feed a 'type' attribute

--- source/_partials/link.blade.php
+++ source/_partials/link.blade.php
@@ -2,4 +2,5 @@
    class="p-url"
    itemprop="url"
    @if ($page->description) title="{{ $page->description }}" @endif
+   @if ($page->type) type="{{ $page->type }}" @endif
    >{{ $page->link_text ?? $page->title }}</a>
