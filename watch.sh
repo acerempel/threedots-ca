96388d3d9721c8f6da4d93e96f105f275ba44d2a
Alan Rempel <alan.rempel@gmail.com>
2021-02-03 08:44:51 -0500

    Watch with Shake

--- watch.sh
+++ watch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 miniserve --index index.html --port 8000 build_local &
 trap 'kill %1' EXIT
-fd . -t f source | entr -d ./vendor/bin/jigsaw build --cache
+fd . -t f source | entr -d ./threedots
