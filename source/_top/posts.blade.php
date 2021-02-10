c4b704e89e54a01d67ee294bcffc2f3de5a3e940
Alan Rempel <alan.rempel@gmail.com>
2021-02-10 10:10:05 -0500

    Add rotated headings in post list

--- source/_top/posts.blade.php
+++ source/_top/posts.blade.php
@@ -11,8 +11,8 @@ weight: 8
           <a class="nav-link" href="#y{{ $year }}">{{ $year }}</a>@unless ($loop->last)<span class="nav-divider">•</span>@endunless
         @endpush
         @push('years_list')
-        <section id="y{{ $year }}" class="border-top article-list">
-          <h2 class="font-size-4 semibold sans-serif">{{ $year }}</h2>
+        <section id="y{{ $year }}" class="article-list mt-3/4">
+          <h2 class="font-size-4 semibold sans-serif swing">{{ $year }}</h2>
             @foreach ($year_posts as $post)
             @include ('_partials.summary', ['post' => $post])
             @endforeach
